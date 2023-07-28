5aeb73c7abef3c4f106a9bb76be52ac4e3273d2e
Arianna Ramon <bananafish@outlook.es>
2023-07-28 09:34:38 -0500

    listado de eventos

--- src/app/pages/crear-ponente/crear-ponente.component.ts
+++ src/app/pages/crear-ponente/crear-ponente.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./crear-ponente.component.css']
 })
 export class CrearPonenteComponent {
+  onFileSelected(event: any) {
+    // Aquí puedes manejar la lógica para cargar la imagen del ponente
+    // Puedes obtener el archivo seleccionado a través de event.target.files[0]
+  }
+
+  crearPonente() {
+    // Lógica para crear al ponente
+    // Puedes obtener los valores de los campos del formulario y enviarlos a través de un servicio o API
+  }
+
+  cancelar() {
+    // Lógica para cancelar la creación del ponente
+    // Por ejemplo, puedes redirigir a otra página o mostrar un mensaje de cancelación
+  }
 
 }
