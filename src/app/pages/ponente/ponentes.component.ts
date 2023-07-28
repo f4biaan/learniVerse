f8bfa83448e5fd16b3e0e53336258e3853d91816
Fabián Montoya <jose.fabian.montoya9@gmail.com>
2023-07-28 14:50:47 -0500

    ponentes events styles

--- src/app/pages/ponente/ponentes.component.ts
+++ src/app/pages/ponente/ponentes.component.ts
@@ -1,28 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FirestoreService,
+  Speaker,
+} from 'src/app/core/services/firestore.service';
 
 @Component({
   selector: 'app-ponentes',
   templateUrl: './ponentes.component.html',
-  styleUrls: ['./ponentes.component.css']
+  styleUrls: ['./ponentes.component.css'],
 })
-export class PonentesComponent {
-  ponentes: any[]; // Suponiendo que los eventos son de tipo any
-  ponentesFiltrados: any[]; // Lista de eventos filtrados por el término de búsqueda
+export class PonentesComponent implements OnInit {
+  ponentes: any[] = []; // Suponiendo que los eventos son de tipo any
+  ponentesFiltrados: any[] = []; // Lista de eventos filtrados por el término de búsqueda
   pageSize = 5; // Tamaño de página (puedes ajustar este valor según tus necesidades)
   currentPage = 1; // Página actual
   showLoading: boolean = false;
-  showFileUploader: boolean = false; 
+  showFileUploader: boolean = false;
   loadingFile: boolean = false;
-  
+  speakers: Speaker[] = [];
 
-  constructor() {
+  constructor(private firestore: FirestoreService) {}
+
+  ngOnInit(): void {
+    this.firestore.getSpeakers().subscribe((speakers) => {
+      this.speakers = speakers;
+    });
+
     // Aquí obtienes los datos de los eventos (puedes hacerlo desde un servicio, API, etc.)
     this.ponentes = [
-      { nombre: 'Ponente 1', cedula: '123456789', email: 'ponente1@gmail.com', profesion: ' Ingeniero' },
-      { nombre: 'Ponente 2', cedula: '123456789', email: 'ponente1@gmail.com', profesion: 'Ingeniero' },
-      { nombre: 'Ponente 3', cedula: '2123456789', email: 'ponente1@gmail.com', profesion: 'Ingeniero' },
-      { nombre: 'Ponente 4', cedula: '123456789', email: 'ponente1@gmail.com', profesion: 'Ingeniero' },
-      { nombre: 'Ponente 5', cedula: '123456789', email: 'ponente1@gmail.com', profesion: 'Ingeniero' },
+      {
+        nombre: 'Ponente 1',
+        cedula: '123456789',
+        email: 'ponente1@gmail.com',
+        profesion: ' Ingeniero',
+      },
+      {
+        nombre: 'Ponente 2',
+        cedula: '123456789',
+        email: 'ponente1@gmail.com',
+        profesion: 'Ingeniero',
+      },
+      {
+        nombre: 'Ponente 3',
+        cedula: '2123456789',
+        email: 'ponente1@gmail.com',
+        profesion: 'Ingeniero',
+      },
+      {
+        nombre: 'Ponente 4',
+        cedula: '123456789',
+        email: 'ponente1@gmail.com',
+        profesion: 'Ingeniero',
+      },
+      {
+        nombre: 'Ponente 5',
+        cedula: '123456789',
+        email: 'ponente1@gmail.com',
+        profesion: 'Ingeniero',
+      },
       // ... Agrega más eventos ...
     ];
     this.ponentesFiltrados = this.ponentes; // Inicialmente, eventosFiltrados es igual a eventos
@@ -47,12 +82,16 @@ export class PonentesComponent {
   filtrarEventos(event: Event) {
     const inputElement = event.target as HTMLInputElement;
     const termino = inputElement.value.trim().toLowerCase(); // Convertir el término de búsqueda a minúsculas
-    this.ponentesFiltrados = this.ponentes.filter(evento => evento.nombre.toLowerCase().includes(termino));
+    this.ponentesFiltrados = this.ponentes.filter((evento) =>
+      evento.nombre.toLowerCase().includes(termino)
+    );
   }
 
   // Genera un array de números desde 1 hasta el número de páginas
   get paginas(): number[] {
     const totalPages = Math.ceil(this.ponentesFiltrados.length / this.pageSize);
-    return Array(totalPages).fill(0).map((x, i) => i + 1);
+    return Array(totalPages)
+      .fill(0)
+      .map((x, i) => i + 1);
   }
 }
