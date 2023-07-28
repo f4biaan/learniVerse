ad9161e8e394d8820c6df7f2e06786e66046d5d9
Fabián Montoya <jose.fabian.montoya9@gmail.com>
2023-07-28 08:19:41 -0500

    trends star component

--- src/app/pages/tendencias/tendencias.component.ts
+++ src/app/pages/tendencias/tendencias.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FirestoreService,
+  Trend,
+} from 'src/app/core/services/firestore.service';
 
 @Component({
   selector: 'app-tendencias',
   templateUrl: './tendencias.component.html',
-  styleUrls: ['./tendencias.component.css']
+  styleUrls: ['./tendencias.component.css'],
 })
-export class TendenciasComponent {
+export class TendenciasComponent implements OnInit {
+  trends: Trend[] = [];
+  tendencias_locales: Trend[] = [];
+  tendencias_nacionales: Trend[] = [];
+  tendencias_globales: Trend[] = [];
 
+  local_filter: Trend[] = [];
+  national_filter: Trend[] = [];
+  global_filter: Trend[] = [];
+
+  constructor(private firestore: FirestoreService) {}
+
+  ngOnInit() {
+    this.firestore.getTrends().subscribe((trends) => {
+      this.trends = trends;
+      // console.log(this.trends);
+      this.tendencias_locales = this.trends.filter((trend) => {
+        return trend.ubicacion === 'local';
+      });
+      this.tendencias_nacionales = this.trends.filter((trend) => {
+        return trend.ubicacion === 'nacional';
+      });
+      this.tendencias_globales = this.trends.filter((trend) => {
+        return trend.ubicacion === 'global';
+      });
+    });
+  }
 }
