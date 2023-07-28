acadc6fb62b42ee662f4db1d3a9720061512e4d9
Fabián Montoya <jose.fabian.montoya9@gmail.com>
2023-07-28 00:22:08 -0500

    Firestore

--- src/app/core/services/firestore.service.ts
+++ src/app/core/services/firestore.service.ts
@@ -7,16 +7,27 @@ import {
 } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
+export interface Event {}
+export interface Speaker {}
+export interface Trend {
+  palabrasClave: string[];
+  temasRelacionados: string[];
+  titulo: string;
+  ubicacion: string;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class FirestoreService {
   private eventsCollection: CollectionReference<any>;
   private speakersCollection: CollectionReference<any>;
+  private trendsCollection: CollectionReference<any>;
 
   constructor(afs: Firestore) {
     this.eventsCollection = collection(afs, 'eventos');
     this.speakersCollection = collection(afs, 'speakers');
+    this.trendsCollection = collection(afs, 'trends');
   }
 
   getEvents = (): Observable<any[]> => {
@@ -25,5 +36,9 @@ export class FirestoreService {
 
   getSpeakers = (): Observable<any[]> => {
     return collectionData(this.speakersCollection);
+  };
+
+  getTrends = (): Observable<Trend[]> => {
+    return collectionData(this.trendsCollection);
   };
 }
